e0d6e28f1bd22d381e739e14547172c9434325f2
MSI <dmitrymorozov29@mail.ru>
2023-12-24 07:45:15 +0300

    Zapisali resheniye zadachi No 2

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 ﻿// Задача 1: Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива
+System.Console.WriteLine("*Reshaem zadachu No 1*");
 char [,] charArray = new char [3,4]
 {
     {'D', 'i', 'm', 'a'},
     {'L', 'e', 'n', 'a'},
     {'V', 'a', 's', 'a'}
 };
-System.Console.WriteLine("Исходный массив: ");
+Console.WriteLine("Исходный массив: ");
 void CreateArray ()
 {
     for (int rows=0; rows < charArray.GetLength(0); rows++)
@@ -14,11 +15,11 @@ void CreateArray ()
         {
             Console.Write(charArray[rows,columns]);
         }
-        System.Console.WriteLine();
+        Console.WriteLine();
     }
 }
 CreateArray();
-System.Console.WriteLine("Строка из элементов массива: ");
+Console.WriteLine("Строка из элементов массива: ");
 void CreateStringFromArray()
 {
     for (int rows=0; rows < charArray.GetLength(0); rows++)
@@ -30,3 +31,23 @@ void CreateStringFromArray()
     }    
 }
 CreateStringFromArray ();
+
+//Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. 
+//Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+Console.WriteLine();
+Console.WriteLine("*Reshaem zadachu No 2*");
+Console.WriteLine("Исходная строка: ");
+string myString = "fjKHNDkndkrhKaioP";
+Console.WriteLine(myString);
+void TransformLetters ()
+{
+    myString = myString.ToLower();
+    // for (int i=0; i==myString.Length; i++)
+    // {
+    //     // string letter;
+    //     letter=myString.Tolower(i);
+    //     System.Console.WriteLine(letter);
+    // }
+    Console.WriteLine("Izmenennaya stroka: " + myString);
+}
+TransformLetters();
