5db57bbc773fa5a28fb2863b7ae038106670a547
MSI <dmitrymorozov29@mail.ru>
2023-12-24 07:11:39 +0300

    Записали решение первой задачи

--- Program.cs
+++ Program.cs
@@ -1,20 +1,32 @@
 ﻿// Задача 1: Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива
 char [,] charArray = new char [3,4]
 {
-    {'d', 'i', 'm', 'a'},
-    {'l', 'e', 'n', 'a'},
-    {'v', 'a', 's', 'a'}
+    {'D', 'i', 'm', 'a'},
+    {'L', 'e', 'n', 'a'},
+    {'V', 'a', 's', 'a'}
 };
+System.Console.WriteLine("Исходный массив: ");
 void CreateArray ()
 {
     for (int rows=0; rows < charArray.GetLength(0); rows++)
     {
         for (int columns=0; columns<charArray.GetLength(1); columns++)
         {
-            // charArray[rows,columns]=charArray[rows,columns++];
             Console.Write(charArray[rows,columns]);
         }
         System.Console.WriteLine();
     }
 }
 CreateArray();
+System.Console.WriteLine("Строка из элементов массива: ");
+void CreateStringFromArray()
+{
+    for (int rows=0; rows < charArray.GetLength(0); rows++)
+    {
+        for (int columns=0; columns<charArray.GetLength(1); columns++)
+        {
+            Console.Write(charArray[rows,columns]);
+        }
+    }    
+}
+CreateStringFromArray ();
